bba6c2ef92e33995c48c78871a8104f84d615624
Hiroyuki JO <jo.hiroyuki@lab.ntt.co.jp>
2018-01-12 16:06:59 +0900

    update install.sh, support Mac OSX

--- install.sh
+++ install.sh
@@ -2,12 +2,6 @@
 
 set -Ceu
 
-readonly SCR_DIR=$(dirname $(readlink -f $0))
-TEST_MODE=false
-LN_OPT="-snvi"
-
-cd $SCR_DIR
-
 function usage() {
     cat << EOT
 Usage:
@@ -22,9 +16,55 @@ EOT
     exit 1
 }
 
+function get_os() {
+    if [ "$(uname)" == "Darwin" ]; then
+        OS="mac"
+    elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+        if [ -e /etc/debian_version ] || [ -e /etc/debian_release ];
+        then
+            if [ -e /etc/lsb-release ]; then
+                OS="ubuntu"
+            else
+                OS="debian"
+            fi
+        elif [ -e /etc/redhat-release ]; then
+            if [ -e /etc/oracle-release ]; then
+                OS="oracle"
+            else
+                OS="redhat"
+            fi
+        elif [ -e /etc/centos-release ]; then
+            OS="centos"
+        else
+            OS="linux"
+        fi
+    else
+        echo "Your platform ($(uname -a)) is no supported."
+        exit 1
+    fi
+}
+
+function get_script_dir() {
+    if [ -L $0 ]; then
+        echo "Please run physical file(not link)."
+        exit 1
+    fi
+    readonly SCR_DIR=$(dirname $0)
+}
+
+OS=""
+TEST_MODE="false"
+LN_OPT="-snvi"
+
+get_os
+echo $OS
+get_script_dir
+
+cd ${SCR_DIR}
+
 while getopts tfh OPT
 do
-    case $OPT in
+    case ${OPT} in
         t) TEST_MODE=true ;;
         f) LN_OPT=${LN_OPT/i/}; LN_OPT+='f' ;;
         h) usage ;;
@@ -34,17 +74,19 @@ done
 
 echo "Install dotfiles..."
 for f in .??*; do
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitignore" ] && continue
-    [ "$f" = ".gitconfig.local.template" ] && continue
-    [ "$f" = ".gitmodules" ] && continue
-    [ ! -e /etc/centos-release ] && [ "$f" = ".bashrc" ] && \
+    [ "${f}" = ".git" ] && continue
+    [ "${f}" = ".gitignore" ] && continue
+    [ "${f}" = ".gitconfig.local.template" ] && continue
+    [ "${f}" = ".gitmodules" ] && continue
+    [ ! ${OS} = "centos" ] && [ "${f}" = ".bashrc" ] && \
         echo Skipping .bashrc, only CentOS are supported. && continue
 
-    if $TEST_MODE; then
-        echo "(test mode) ln $LN_OPT $SCR_DIR/$f ~/$f"
+    if ${TEST_MODE}; then
+        echo "(test mode) ln ${LN_OPT} ${SCR_DIR}/${f} ~/${f}"
     else
-        ln $LN_OPT $SCR_DIR/$f ~/
+        set +e
+        ln ${LN_OPT} ${SCR_DIR}/${f} ~/${f}
+        set -e
     fi
 done
 
